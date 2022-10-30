f062e70d6c915404b93fceb1261cbb60f083fa13
goelshivi04 <95372572+goelshivi04@users.noreply.github.com>
2022-10-30 20:34:18 +0530

    Create roman_to_int.cpp

---
+++ roman_to_int.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+class Solution {
+public: 
+    int romanToInt(string s) {
+        int sum=0;
+        unordered_map<char,int>mp{
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+        };
+        
+        int len=s.size();
+        for(int i=0;i<len;i++)
+        {
+            if(mp[s[i]]<mp[s[i+1]])
+            sum=sum-mp[s[i]];
+            else
+                sum=sum+mp[s[i]];
+        }
+        
+        return sum;
+    }
+};
