3f8d38dd39aa130bcf92ea24acb7e0e2c94dd513
goelshivi04 <95372572+goelshivi04@users.noreply.github.com>
2022-11-06 18:54:43 +0530

    Create binary_search.cpp

---
+++ 6-11-22/binary_search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1,m;
+        m=(l+r)/2;
+        while(l<=r)
+        {
+            m=(l+r)/2;
+            if(nums[m]==target)
+            return m;
+            else if(nums[m]>target)
+            r=m-1;
+            else
+            l=m+1;
+        }
+        return -1;
+    }
+};
