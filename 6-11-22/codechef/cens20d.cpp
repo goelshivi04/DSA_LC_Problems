709e2984b59b766d1555560cb95bcf989f06af5a
goelshivi04 <95372572+goelshivi04@users.noreply.github.com>
2022-11-06 21:01:56 +0530

    Create cens20d.cpp

---
+++ 6-11-22/codechef/cens20d.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,c=0;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	    cin>>a[i];
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<n;j++)
+	        {
+	            if(i<j && a[i]==(a[i] & a[j]))
+	            c++;
+	        }
+	    }
+	    cout<<c<<endl;
+	}
+	return 0;
+}
