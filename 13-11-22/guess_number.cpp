8db6432fc51c520861efec824e004865b6b0626d
goelshivi04 <95372572+goelshivi04@users.noreply.github.com>
2022-11-13 19:44:40 +0530

    Create guess_number.cpp

---
+++ 13-11-22/guess_number.cpp
@@ -0,0 +1,23 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+      int left=1, right=n, middle=n/2, ret;
+    while ((ret=guess(middle)) != 0) { 
+        if(ret == -1) 
+        right = middle-1;
+        else
+        left = middle+1;
+        middle = left + (right-left)/2;
+    }
+    return middle;  
+    }
+};
