0ae12641c7c21e07650c09f053ea371d6e46a953
goelshivi04 <95372572+goelshivi04@users.noreply.github.com>
2022-11-19 19:35:06 +0530

    Create root_sum.cpp

---
+++ 19-11-22/root_sum.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        int l=root->left->val;
+        int r=root->right->val;
+        if(root->val==l+r)
+        return true;
+        else
+        return false;
+    }
+};
