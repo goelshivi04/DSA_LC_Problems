9648ffbf48e5f8c0c619a81d04dfc6c4cb92ed34
goelshivi04 <95372572+goelshivi04@users.noreply.github.com>
2022-12-01 16:29:56 +0530

    Create p1.cpp

---
+++ 1-12-22/p1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int count=0,a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    if((a+b+c) <= d)
+	    cout<<1<<endl;
+	    else if((a+b)<=d ||(c+b)<=d || (a+c)<=d)
+	    cout<<2<<endl;
+	    else
+	    cout<<3<<endl;
+	}
+	return 0;
+}
