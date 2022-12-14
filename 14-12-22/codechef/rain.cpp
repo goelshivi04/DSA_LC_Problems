844a06099779fdcb7d7786d891895f416c73e589
goelshivi04 <95372572+goelshivi04@users.noreply.github.com>
2022-12-14 20:57:29 +0530

    Create rain.cpp

---
+++ 14-12-22/codechef/rain.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    if(n<3)
+	    cout<<"LIGHT"<<endl;
+	    else if(n>=3 && n<7)
+	    cout<<"MODERATE"<<endl;
+	    else
+	    cout<<"HEAVY"<<endl;
+	}
+	return 0;
+}
