b9660e83f15fbcf18487f1a808cd1a992152b75b
goelshivi04 <95372572+goelshivi04@users.noreply.github.com>
2022-12-16 14:52:48 +0530

    Create ll_cycle.cpp

---
+++ 16-12-22/ll_cycle.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *temp;
+        if(head==NULL)
+        return NULL;
+
+        while(head)
+        {
+            if(head->val==INT_MIN)
+            return head;
+            
+            head->val=INT_MIN;
+            head=head->next;
+        }
+        return NULL;
+    }
+};
